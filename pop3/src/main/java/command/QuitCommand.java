f804f2a5345b6ab36105192b8c62e5477674f32b
nss1024 <32052333+nss1024@users.noreply.github.com>
2026-04-04 18:56:52 +0100

    implemented QUIT commands and tested via telnet

--- pop3/src/main/java/command/QuitCommand.java
+++ pop3/src/main/java/command/QuitCommand.java
@@ -1,23 +1,51 @@
 package command;
 
 import authenticate.AuthState;
+import mailbox.MailMessage;
+import mailbox.Mailbox;
 import mailbox.MailboxManager;
 import parser.Pop3Request;
 import response.Pop3Response;
 import session.SessionContext;
+import session.SessionState;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class QuitCommand implements Pop3Command{
 
     @Override
     public Pop3Response execute(Pop3Request request, SessionContext context) {
+        MailboxManager mailboxManager = context.getMailboxManager();
 
-              
-        if(context.getAuthState()== AuthState.LOGGED_OUT){
+        if (context.getAuthState() == AuthState.LOGGED_OUT) {
             return Pop3Response.ok("");
         }
 
+        if (context.getSessionState() == SessionState.TRANSACTION) {
+            handleTransactionQuit(context);
+            mailboxManager.unlock(context.getUserName(), context.getSessionId());
+        }
 
+        return Pop3Response.ok("bye");
 
-        return null;
     }
+
+private void handleTransactionQuit(SessionContext context) {
+    Mailbox mailbox = context.getMailbox();
+
+    for (MailMessage msg : mailbox.getMessages()) {
+        if (msg.isDeleted()) {
+            try {
+                Files.deleteIfExists(msg.getFilePath());
+            } catch (IOException e) {
+                // log it, but don't keep QUIT moving
+                System.err.println("Failed to delete: " + msg.getFilePath());
+            }
+        }
+    }
+
+
 }
+}
+
