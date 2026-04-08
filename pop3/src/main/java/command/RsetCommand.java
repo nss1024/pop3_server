439bdbd238c8eb4752f764effca727e88aebf82d
nss1024 <32052333+nss1024@users.noreply.github.com>
2026-04-08 17:40:16 +0100

    Added rset implementation. The command sets deleted property of MailMessage instance, the abstraction of an email to false, that is, mail will not be deleted on QUIT

--- pop3/src/main/java/command/RsetCommand.java
+++ pop3/src/main/java/command/RsetCommand.java
@@ -1,13 +1,29 @@
 package command;
 
+import mailbox.MailMessage;
+import mailbox.Mailbox;
 import parser.Pop3Request;
 import response.Pop3Response;
 import session.SessionContext;
+import session.SessionState;
 
 public class RsetCommand implements Pop3Command{
 
     @Override
     public Pop3Response execute(Pop3Request request, SessionContext context) {
-        return null;
+        if (context.getSessionState() != SessionState.TRANSACTION) {
+            return Pop3Response.err("not in transaction state");
+        }
+
+        Mailbox mailbox = context.getMailbox();
+
+        for (MailMessage msg : mailbox.getMessages()) {
+            if (msg.isDeleted()) {
+                msg.setDeleted(false);
+            }
+        }
+
+        return Pop3Response.ok("maildrop has been reset");
+
     }
 }
