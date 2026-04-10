a214563c87223bfe8eff6ba2a3c00e01409be55a
nss1024 <32052333+nss1024@users.noreply.github.com>
2026-04-10 17:18:38 +0100

    Add - started implementing TOP command, read top n lines from a message body.

--- pop3/src/main/java/command/TopCommand.java
+++ pop3/src/main/java/command/TopCommand.java
@@ -1,13 +1,83 @@
 package command;
 
+import mailbox.MailMessage;
 import parser.Pop3Request;
 import response.Pop3Response;
 import session.SessionContext;
+import session.SessionState;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TopCommand implements Pop3Command{
 
     @Override
     public Pop3Response execute(Pop3Request request, SessionContext context) {
-        return null;
+
+        if (context.getSessionState() != SessionState.TRANSACTION) {
+            return Pop3Response.err("not in transaction state");
+        }
+        String arg = request.getArgument();
+
+        if (arg == null || arg.isBlank()) {
+            return Pop3Response.err("missing message number");
+        }
+
+        int index;
+        try {
+            index = Integer.parseInt(arg);
+        } catch (NumberFormatException e) {
+            return Pop3Response.err("invalid message number");
+        }
+
+        MailMessage message = context.getMailbox().getMessage(index);
+
+        if (message == null || message.isDeleted()) {
+            return Pop3Response.err("no such message");
+        }
+
+        List<String> lines = new ArrayList<>();
+        lines.add("+OK");
+
+        boolean inBody = false;
+        int bodyCount = 0;
+
+        try (BufferedReader reader = Files.newBufferedReader(message.getFilePath())) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                // dot-stuffing
+                if (line.startsWith(".")) {
+                    line = "." + line;
+                }
+
+                if (!inBody) {
+                    lines.add(line);
+
+                    if (line.isEmpty()) {
+                        inBody = true;
+                    }
+
+                } else {
+                    if (bodyCount < 5) {
+                        lines.add(line);
+                        bodyCount++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            return Pop3Response.err("failed to read message");
+        }
+
+        return Pop3Response.multiLine(lines);
+
     }
 }
